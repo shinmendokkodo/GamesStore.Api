3d967857c7c84e0c22eac8311b4221a0250cc2af
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-05-16 01:16:28 +0530

    setup user-secrets

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using GamesStore.Api.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
 
+var conn = builder.Configuration.GetConnectionString("GameStoreContext");
+
 var app = builder.Build();
 
 app.MapEndPoints();
